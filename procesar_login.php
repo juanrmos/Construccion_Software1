1ee565077ea2a14f3b74ad142fe22c460d0ffbfa
juanrmos <juanangelfabian282@gmail.com>
2024-11-09 15:50:30 -0500

    Actualizacion de inicio de sesion

--- procesar_login.php
+++ procesar_login.php
@@ -1,13 +1,27 @@
 <?php
 // procesar_login.php
+session_start();
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
     if ($usuario === "usuario" && $contrasena === "contrasena123") {
-        echo "Bienvenido, $usuario!";
+        $_SESSION['usuario'] = $usuario;
+        $_SESSION['contrasena'] = $contrasena;
+        header('location: http://127.0.0.1/ExamenMC/Principal.php');
+        exit();
+
     } else {
-        echo "Usuario o contraseña incorrectos.";
+        header('location: http://127.0.0.1/ExamenMC/clave_error.php');
+        $_SESSION['usuario'] = $usuario;
+        exit();
     }
+    //Cargar nuevamente las variables al volver a pagina de inicio.
 }
+
+
 ?>
+
